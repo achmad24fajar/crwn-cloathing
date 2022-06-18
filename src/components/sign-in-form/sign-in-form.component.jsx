7e6c4990cad5537e49568305075c6b4f27a0c720
achmad24fajar <achmad24fajar@gmail.com>
2022-06-18 18:59:37 +0700

    update firebase authentication

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+import {
+  auth,
+  signInWithGooglePopup,
+  getUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  createAuthUserWithEmailAndPassword,
+  signInAuthUserWithEmailAndPassword
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-in-form.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+};
+
+const SignIn = () => {
+  // useEffect(() => {
+  //   gettingRedirectResult();
+  // }, []);
+
+  // const gettingRedirectResult = async () => {
+  //   const response = await getRedirectResult(auth);
+  //   if(response){
+  //     const userDocRef = await getUserDocumentFromAuth(response.user);
+  //   }
+  // };
+
+  const [formField, setFormField] = useState(defaultFormFields);
+  const { email, password } = formField;
+
+  console.log(formField);
+
+  const logGoogleUser = async () => {
+    await signInWithGooglePopup();
+  };
+
+  const resetFormField = () => {
+    setFormField(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+      resetFormField()
+    } catch (err) {
+      console.log(err.message)
+      switch(err.message){
+        case 'Firebase: Error (auth/wrong-password).':
+          alert('Your password is wrong!')
+          break;
+        case 'Firebase: Error (auth/user-not-found).':
+          alert('Your email does not match');
+          break;
+
+        default:
+          console.log(err.message)
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormField({ ...formField, [name]: value });
+  };
+
+  return (
+    <div className='sign-up-container'>
+      {/* <button onClick={logGoogleUser}>Sign in with googke popup</button> */}
+      <h2>Already have an account</h2>
+      <span>Sign in with your email and password.</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+          <Button type="button" buttonType='google' onClick={logGoogleUser}>
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,28 +1,37 @@
-import React, {Fragment} from 'react'
-import { Outlet, Link } from 'react-router-dom'
-import {ReactComponent as Crwn} from '../../assets/crown.svg'
+import React, { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as Crwn } from '../../assets/crown.svg';
 
-import './navigation.styles.scss'
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
-        <div className='navigation'>
-            <Link to='/' className='logo-container'>
-                <Crwn />
+      <div className='navigation'>
+        <Link to='/' className='logo-container'>
+          <Crwn />
+        </Link>
+        <div className='nav-links-container'>
+          <Link to='/shop' className='nav-link'>
+            Shop
+          </Link>
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+          ) : (
+            <Link to='/auth' className='nav-link'>
+              Sign In
             </Link>
-            <div className='nav-links-container'>
-                <Link to='/shop' className='nav-link'>
-                    Shop
-                </Link>
-                <Link to='/sign-in' className='nav-link'>
-                    Sign In
-                </Link>
-            </div>
+          )}
         </div>
-        <Outlet />
+      </div>
+      <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
