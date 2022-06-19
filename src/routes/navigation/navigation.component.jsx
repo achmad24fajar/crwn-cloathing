76b2b331e41eff8a0db4327c74acb527ab1b2838
achmad24fajar <achmad24fajar@gmail.com>
2022-06-19 19:21:39 +0700

    create cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,13 +2,19 @@ import React, { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as Crwn } from '../../assets/crown.svg';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
 
   return (
     <Fragment>
@@ -18,7 +24,7 @@ const Navigation = () => {
         </Link>
         <div className='nav-links-container'>
           <Link to='/shop' className='nav-link'>
-            Shop
+            SHOP
           </Link>
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
@@ -27,7 +33,9 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && (<CartDropdown />)}
       </div>
       <Outlet />
     </Fragment>
